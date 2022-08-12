939c6dc9c57b9c2fbb31b0121786a6c566e60034
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-08-12 19:36:28 +0900

    お勉強のためにTestケースをかいてみる

---
+++ packages/vue-material-symbols/src/test/GSymbol.spec.ts
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils'
+import GSymbol from '../components/GSymbol.vue'
+import { test, expect } from 'vitest';
+
+test('icon', () => {
+  const icon = 'cake';
+
+  const gSymbol = mount(GSymbol, {
+    props: {
+      icon
+    },
+  });
+
+  expect(gSymbol.text()).equal(icon)
+});
--- packages/vue-material-symbols/vite.config.ts
+++ packages/vue-material-symbols/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -20,5 +22,8 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    environment: 'happy-dom'
+  }
 });
